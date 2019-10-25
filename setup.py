f97fedda63f82b588de93ded51e0e4236a6b62ae
egichuri <egaturu@gmail.com>
2019-10-25 15:02:46 +0300

    Mirror: 0.12.0

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from setuptools import setup
 setup(
     name='pre_commit_dummy_package',
     version='0.0.0',
-    install_requires=['future==0.11.4'],
+    install_requires=['future==0.12.0'],
 )
